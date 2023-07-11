9ff8d5b54160721875bc5b0e4e5b4807c6606202
Imran Hossain <blur-imran@hotmail.com>
2023-07-11 12:41:17 +0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'users/index', to: 'users#index'
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
