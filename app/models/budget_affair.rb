1c60f3e2816bfd694ef9226c9dea61d41f36f406
Imran Hossain <blur-imran@hotmail.com>
2023-07-13 20:46:17 +0600

    Update budget_affair.rb

--- app/models/budget_affair.rb
+++ app/models/budget_affair.rb
@@ -1,6 +1,6 @@
 class BudgetAffair < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many : budget_types, through: :type_affairs, dependent: :destroy
+  has_many :budget_types, through: :type_affairs, dependent: :destroy
   
   validates :name, presence: true
   validates :amount, numericality: true
