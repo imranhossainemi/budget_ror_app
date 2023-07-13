d74db42dd8fd54352e35e5998aa4bc6fc974f494
Imran Hossain <blur-imran@hotmail.com>
2023-07-13 14:27:40 +0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :budget_types, foreign_key: 'author_id', dependent: :destroy
   validates :email, presence: true
   validates :name, presence: true
 end
