2036cb372718f930f6745e7742ae017e636d1ac2
Imran Hossain <blur-imran@hotmail.com>
2023-07-13 20:46:29 +0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions/sessions',
-    registrations: 'users/sessions/registrations'
-  }
+  devise_for :users
   resources :users
-  resources :budget_types, excpt: [:update, :edit]
+  resources :budget_types, excpt: [:update, :edit] do 
+    resources :budget_affairs, except: [:update, :edit]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
