f9e7e697a3b7c3ce3cc8bd6005e5fd69c212e3b8
Imran Hossain <blur-imran@hotmail.com>
2023-07-13 11:02:38 +0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'budget_types/index'
+  get 'budget_types/show'
+  get 'budget_types/new'
   devise_for :users
   resources :users
+  resources :budget_types
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
