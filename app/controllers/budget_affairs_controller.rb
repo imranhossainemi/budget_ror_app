f2f79cc099ad36f055ea22ef9dffe55fd4a51a79
Imran Hossain <blur-imran@hotmail.com>
2023-07-14 14:09:53 +0600

    Update budget_affairs_controller.rb

--- app/controllers/budget_affairs_controller.rb
+++ app/controllers/budget_affairs_controller.rb
@@ -1,16 +1,16 @@
 class BudgetAffairsController < ApplicationController
-  def new 
+  def new
     @affair = BudgetAffair.new
   end
 
-  def create 
-    @affair = BudgetAffair.new(name: budget_affair_params[:name], amount: budget_affair_params[:amount])
-
+  def create
+    @affair = BudgetAffair.new(budget_affair_params)
     @affair.author_id = current_user.id
+
     if @affair.save
-      TypeAffair.create(budget_affair: @affair, budget_type_id: budget_affair_params[:budget_type_id])
-      flash[:success] = 'New Payment Affair succesfully Added'
-      redirect_to budget_type_path(budget_affair_params[:budget_type_id])
+      TypeAffair.create(budget_affair: @affair, budget_type_id: params[:budget_affair][:budget_type_id])
+      flash[:success] = 'New Payment Affair successfully added'
+      redirect_to budget_type_path(params[:budget_affair][:budget_type_id])
     else
       flash[:error] = 'Error: new payment affair could not be added'
       render :new
