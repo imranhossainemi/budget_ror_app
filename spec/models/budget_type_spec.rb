92e3e506d40ffdb8525fd3bb9351337a55bf9eb1
Imran Hossain <blur-imran@hotmail.com>
2023-07-14 20:53:29 +0600

    Update budget_type_spec.rb

--- spec/models/budget_type_spec.rb
+++ spec/models/budget_type_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe BudgetType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.create(name: 'test', password: 'testpass', email: 'test@test.com')
+    @category = BudgetType.new(name: 'test', icon: 'test')
+  end
+
+  describe 'validate data' do
+    it 'it should have a name' do
+      @category.name = nil
+      expect(@category).to_not be_valid
+    end
+
+    it 'should have an icon' do
+      @category.icon = nil
+      expect(@category).to_not be_valid
+    end
+  end
 end
