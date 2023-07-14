98ea74cf712ed773068bde8872c4233cc8aa36ed
Imran Hossain <blur-imran@hotmail.com>
2023-07-14 20:53:54 +0600

    Update new.html.erb_spec.rb

--- spec/views/budget_types/new.html.erb_spec.rb
+++ spec/views/budget_types/new.html.erb_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'budget_types/new.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include Devise::Test::IntegrationHelpers
+  describe 'New' do
+    before(:each) do
+      @user = User.create(name: 'Imran', email: 'imranemi@mail.com', password: 123_456)
+      sign_in @user
+      visit ('/budget_types/new')
+      fill_in 'budget_type[name]', with: 'test group'
+      fill_in 'budget_type[icon]', with: 'test icon'
+      click_button 'Create'
+    end
+    it 'Group name' do
+      expect(page).to have_content('test group')
+    end
+    it 'Group Icon' do
+      expect(page).to have_content('test group')
+    end
+  end
 end
