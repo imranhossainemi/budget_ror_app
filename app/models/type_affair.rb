8c25d546296577a4fc1d23c83f1289dadd2abcff
Imran Hossain <blur-imran@hotmail.com>
2023-07-14 14:10:01 +0600

    Update type_affair.rb

--- app/models/type_affair.rb
+++ app/models/type_affair.rb
@@ -1,4 +1,4 @@
 class TypeAffair < ApplicationRecord
-  belongs_to :budget_type
-  belongs_to :budget_affair
+  belongs_to :budget_type, class_name: 'BudgetType'
+  belongs_to :budget_affair, class_name: 'BudgetAffair'
 end
