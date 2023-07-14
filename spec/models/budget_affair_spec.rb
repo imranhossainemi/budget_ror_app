a8d0331b5dcfa15f15688d22360e040bf1199838
Imran Hossain <blur-imran@hotmail.com>
2023-07-14 20:53:26 +0600

    Update budget_affair_spec.rb

--- spec/models/budget_affair_spec.rb
+++ spec/models/budget_affair_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe BudgetAffair, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.create(name: 'test', password: 'testpass', email: 'test@test.com')
+    @category = BudgetType.create(author: @user, name: 'Food', icon: 'food')
+    @payment = BudgetAffair.new(name: 'test', amount: 100)
+  end
+
+  describe 'validate data' do
+    it 'it should have a name' do
+      @payment.name = nil
+      expect(@payment).to_not be_valid
+    end
+
+    it 'should have an amount' do
+      @payment.amount = nil
+      expect(@payment).to_not be_valid
+    end
+
+    it 'the amount should be numerical' do
+      @payment.amount = 'a'
+      expect(@payment).to_not be_valid
+    end
+  end
 end
