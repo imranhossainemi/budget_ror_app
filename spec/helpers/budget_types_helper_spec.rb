31c1de4556351df2f0fa6fa7d76781d31d7acbd7
Imran Hossain <blur-imran@hotmail.com>
2023-07-14 20:53:21 +0600

    Update budget_types_helper_spec.rb

--- spec/helpers/budget_types_helper_spec.rb
+++ spec/helpers/budget_types_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe BudgetTypesHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe BudgetTypesHelper, type: :helper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
